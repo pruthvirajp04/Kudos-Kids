f7d3c36510778fcbe76237a5ed125eee384e568a
Pruthviraj Patil <95764252+pruthvirajp04@users.noreply.github.com>
2023-01-11 17:16:15 +0530

    slider added

--- script.js
+++ script.js
@@ -1,41 +1,18 @@
+var img = document.getElementById('img');
 
-var swiper = new Swiper(".home-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2,
-    slideShadows: true,
-  },
-  loop:true,
-  autoplay:{
-    delay: 3000,
-    disableOnInteraction:false,
-  }
-});
+var slides=['images/img002.jpeg','images/img003.jpeg', 'images/img004.jpeg','images/img005.jpeg', 'images/img006.jpeg', 'images/img007.jpeg'];
 
-var swiper = new Swiper(".review-slider", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop:true,
-    spaceBetween: 10,
-    breakpoints: {
-      0: {
-          slidesPerView: 1,
-      },
-      700: {
-        slidesPerView: 2,
-      },
-      1050: {
-        slidesPerView: 3,
-      },    
-    },
-    autoplay:{
-      delay: 5000,
-      disableOnInteraction:false,
-  }
-});
+var Start=0;
+
+function slider(){
+    if(Start<slides.length){
+        Start=Start+1;
+    }
+    else{
+        Start=1;
+    }
+    console.log(img);
+    img.innerHTML = "<img src=" + slides[Start-1]+">";
+   
+}
+setInterval(slider,2000);
