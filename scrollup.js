d9793f75f39375e86148f0cc6c9720a605bcffaf
Pruthviraj Patil <95764252+pruthvirajp04@users.noreply.github.com>
2023-01-24 23:37:26 +0530

    top added

--- scrollup.js
+++ scrollup.js
@@ -1,19 +1,8 @@
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
   } else {
-    mybutton.style.display = "none";
+    x.style.display = "block";
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
